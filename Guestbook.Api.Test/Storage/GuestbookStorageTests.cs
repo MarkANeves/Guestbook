36b4fddb47c0cefadb672c06c4316dba1d548ea0
Alex <3845319+alexjneves@users.noreply.github.com>
2021-04-27 15:29:14 -0700

    Test project, Storage tests

---
+++ Guestbook.Api.Test/Storage/GuestbookStorageTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Guestbook.Api.Storage;
+using NUnit.Framework;
+
+namespace Guestbook.Api.Test.Storage
+{
+    public abstract class GuestbookStorageTests
+    {
+        private IGuestbookStorage _storage;
+
+        protected abstract IGuestbookStorage CreateStorage();
+
+        [SetUp]
+        public void Setup()
+        {
+            _storage = CreateStorage();
+        }
+
+        [Test]
+        public async Task CanReadWriteAndClear()
+        {
+            var now = DateTime.UtcNow;
+
+            await _storage.AddEntry(new Models.GuestbookEntry("1", now));
+            await _storage.AddEntry(new Models.GuestbookEntry("2", now));
+            await _storage.AddEntry(new Models.GuestbookEntry("3", now));
+
+            var guestbook = await _storage.GetGuestbook();
+
+            guestbook.Entries.Should().HaveCount(3)
+                .And.BeEquivalentTo(
+                    new Models.GuestbookEntry("1", now),
+                    new Models.GuestbookEntry("2", now),
+                    new Models.GuestbookEntry("3", now)
+                );
+
+            await _storage.ClearGuestbook();
+
+            var guestbookAfterClear = await _storage.GetGuestbook();
+
+            guestbookAfterClear.Entries.Should().BeEmpty();
+        }
+    }
+}
---
+++ Guestbook.Api.Test/Storage/InMemoryGuestbookStorageTests.cs
@@ -0,0 +1,9 @@
+﻿using Guestbook.Api.Storage;
+
+namespace Guestbook.Api.Test.Storage
+{
+    public class InMemoryGuestbookStorageTests : GuestbookStorageTests
+    {
+        protected override IGuestbookStorage CreateStorage() => new InMemoryGuestbookStorage();
+    }
+}
